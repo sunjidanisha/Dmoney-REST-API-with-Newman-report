fa0f2c3bb9f38670f62a56846d98ef09ad836b34
sunjidanisha <saniharifa@gmail.com>
2024-11-29 20:52:23 +0600

    first commit

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    collection:`https://api.postman.com/collections/33497181-42dda570-4d07-4cdb-a48a-4fa432796337?access_key=PMAT-01JDVZKZVKZN80GHDYR9K60F4M`,
+    //collection:require('./collection/collection.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    //environment: './env.json',
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
